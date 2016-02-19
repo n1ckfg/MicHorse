733210c95d10580ca8996dbe13b5871f21ce2554
Nick Fox-Gieg <nick@fox-gieg.com>
2016-02-19 01:11:38 -0500

    x

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,18 @@
 #include "ofMain.h"
 #include "ofApp.h"
 
+#ifdef TARGET_RASPBERRY_PI
+    #include "ofGLProgrammableRenderer.h"
+#endif
+
 int main() {
     
     ofSetLogLevel(OF_LOG_VERBOSE);
+    
+    #ifdef TARGET_RASPBERRY_PI
+        ofSetCurrentRenderer(ofGLProgrammableRenderer::TYPE);
+    #endif
+    
     ofSetupOpenGL(1280, 720, OF_FULLSCREEN);
     ofRunApp(new ofApp());
     
