cb82f23c1200944d66d2953b2b561adfe64482df
Nick Fox-Gieg <nick@fox-gieg.com>
2020-03-21 14:39:58 -0400

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,17 @@
 #include "ofMain.h"
 #include "ofApp.h"
-#include "ofGLProgrammableRenderer.h"
 
+//========================================================================
 int main() {
-    
-    ofSetLogLevel(OF_LOG_VERBOSE);
-    ofSetLogLevel("ofThread", OF_LOG_ERROR);
 
-    #ifdef TARGET_OPENGLES
-        ofSetCurrentRenderer(ofGLProgrammableRenderer::TYPE);
-    #endif
+    ofGLESWindowSettings settings;
+	settings.glesVersion = 2;
+	settings.windowMode = OF_FULLSCREEN;
+	ofCreateWindow(settings);
     
-    ofSetupOpenGL(1280, 720, OF_FULLSCREEN);
-    ofRunApp(new ofApp());
-    
+	// this kicks off the running of my app
+	// can be OF_WINDOW or OF_FULLSCREEN
+	// pass in width and height too:
+	ofRunApp(new ofApp());
+
 }
